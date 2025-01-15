3fea9d720071262e7e35ae62f0cddb836550954c
user638726 <lienjackson35@gmail.com>
2025-01-15 16:35:18 +0800

    嘗試放在中間

--- WEB04/index.php
+++ WEB04/index.php
@@ -14,20 +14,10 @@
 </head>
 
 <body>
-    <div id="cover" style="display:none; ">
-        <div id="coverr">
-            <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;"
-                onclick="cl(&#39;#cover&#39;)">X</a>
-            <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
-        </div>
-    </div>
-   
+
+
     <div id="main">
-        <a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
-            
-            <div class="ti" style="background:url('./upload/<?=$Title->find(['sh'=>1])['img'];?>'); background-size:cover;"></div>
-            <!--標題-->
-        </a>
+
         <div id="ms">
             <div id="lf" style="float:left;">
                 <div id="menuput" class="dbor">
@@ -61,19 +51,19 @@
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :
-                    <?=$Total->find(1)['total'];?></span>
+                        <?=$Total->find(1)['total'];?></span>
                 </div>
             </div>
-			<?php
+            <?php
 				//$do=$_GET['do']??'main';
 
 				$do=$_GET['do']??'main';
-				$file="./front/{$do}.php";
+				$file="./front/{$do}.html";
 
 				if(file_exists($file)){
 					include $file;
 				}else{
-					include "./front/main.php";
+					include "./front/天氣預報.html";
 				}
 
 				//include (file_exists($file))?$file:"./front/main.php";
@@ -89,59 +79,20 @@
                 <?php 
                     }else{
                 ?>
-                    <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;"
+                <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;"
                     onclick="lo(&#39;admin.php&#39;)">返回管理</button>
                 <?php 
                     }
                 ?>
-                <div style="width:89%; height:480px;" class="dbor">
-                    <span class="t botli">校園映象區</span>
-                    <div class='cent' id="up" onclick="pp(1)">
-                        <img src="./icon/up.jpg" alt="" srcset="">
-                    </div>
-                    <div class='cent'>
-                    <?php 
-                    $imgs=$Image->all(['sh'=>1]);
-                    foreach($imgs as $idx => $img){
-                        echo "<div class='im' id='ssaa{$idx}'>";
-                        echo "<img src='./upload/{$img['img']}' style='width:150px;height:103px;border:3px solid orange'>";
-                        echo "</div>";
-                    }
 
-                        ?>
-                    </div>
-                    <div class='cent' id="dn" onclick="pp(2)">
-                        <img src="./icon/dn.jpg" alt="">
-                    </div>
-
-                    <script>
-                    var nowpage = 0,
-                        num = <?=$Image->count(['sh'=>1]);?>;
-
-                    function pp(x) {
-                        var s, t;
-                        if (x == 1 && nowpage - 1 >= 0) {
-                            nowpage--;
-                        }
-                        if (x == 2 && (nowpage + 1)<= num * 1 - 3) {
-                            nowpage++;
-                        }
-                        $(".im").hide()
-                        for (s = 0; s <= 2; s++) {
-                            t = s * 1 + nowpage * 1;
-                            $("#ssaa" + t).show()
-                        }
-                    }
-                    pp(1)
-                    </script>
-                </div>
             </div>
         </div>
-        <div style="clear:both;"></div>
-        <div
-            style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-            <span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
-        </div>
+    </div>
+
+    <div
+        style="width:1024px; left:0px; background:#FC3; margin:0 auto; height:123px; display:block; position:absolute; top:658px; left:73px;">
+        <span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
+    </div>
     </div>
 
 </body>
