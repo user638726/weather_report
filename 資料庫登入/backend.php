2cec8013693ae4a8d0d91ae1f6ffaced7c755b81
user638726 <lienjackson35@gmail.com>
2025-01-17 16:36:37 +0800

    增加功能

--- 資料庫登入/backend.php
+++ 資料庫登入/backend.php
@@ -13,19 +13,43 @@
 
 <body>
     <h2>歡迎來到後台!</h2>
-    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=total">
-        <div class="mainmu">
-            進站總人數管理 </div>
-    </a>
-    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=bottom">
-        <div class="mainmu">
-            頁尾版權資料管理 </div>
-    </a>
-    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin">
-        <div class="mainmu">
-            管理者帳號管理 </div>
-    </a>
-    <p><a href="logout.php">登出</a></p>
+    <div id="ms">
+        <div id="lf" style="float:left;">
+            <div id="menuput" class="dbor">
+                <!--主選單放此-->
+                <span class="t botli">後台管理選單</span>
+                <a style="color:#000; font-size:13px; text-decoration:none" href="?do=total">
+                    <div class="mainmu">
+                        進站總人數管理 </div>
+                </a>
+                <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=bottom">
+                    <div class="mainmu">
+                        頁尾版權資料管理 </div>
+                </a>
+                <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin">
+                    <div class="mainmu">
+                        管理者帳號管理 </div>
+                </a>
+                <div style="height:32px; display:block;"></div>
+                <!--正中央-->
+                <div style="width:100%; padding:2px; height:290px;">
+                    <div id="mwww" loop="true" style="width:100%; height:100%;">
+                        <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
+                    </div>
+                </div>
+                <script>
+                function ww() {
+                    // console.log("HI");
+                    $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+                    //$("#mwww").attr("src",lin[now])
+                    now++;
+                    if (now >= lin.length)
+                        now = 0;
+                }
+
+                ww();
+                </script>
+                <p><a href="logout.php">登出</a></p>
 </body>
 
 </html>
