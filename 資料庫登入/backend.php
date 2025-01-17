18a728a0473e17a098c13fb12fae07fb0d32b797
user638726 <lienjackson35@gmail.com>
2025-01-17 14:44:56 +0800

    導向後台

---
+++ 資料庫登入/backend.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>後台管理</title>
+</head>
+
+<body>
+    <h2>歡迎來到後台!</h2>
+    <p><a href="logout.php">登出</a></p>
+</body>
+
+</html>
